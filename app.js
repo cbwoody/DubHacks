8993b1d273f4e8f7e05aedb0d1feb97205724a7c
Cole <woodford.cole@gmail.com>
2017-10-21 23:33:12 -0700

    Completed tutorial for echo bot.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var builder = require('botbuilder');
 //Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-	console.log('%s listening to %s", server.name, server.url);
+	console.log('%s listening to %s', server.name, server.url);
 });
 
 //Create chat connector for communicating with the Bot Framework Service

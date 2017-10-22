2e2504318832de6d9bc1be098e1789ad305238b8
Ryan <rjoyce@champtc.com>
2017-10-22 00:46:14 -0700

    booya

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-
-
 var restify = require('restify');
 var builder = require('botbuilder');
+var request = require('request');
 
-//Setup Restify Server
+let options = {
+  "url": "https://apis.solarialabs.com/shine/v1/vehicle-thefts",
+  "method": "GET",
+  "qs": {
+    "state": "wa",
+    "apikey": "key"
+  }
+}
+
+// Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-	console.log('%s listening to %s', server.name, server.url);
+   console.log('%s listening to %s', server.name, server.url); 
 });
 
-//Create chat connector for communicating with the Bot Framework Service
+// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-	appID: process.env.MICROSOFT_APP_ID,
-	appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-//Listen for messages from users
+// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-//Recieve messages from the user and respond by echoing each message back (prefixed with 'You said:')
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-	session.send("You said: %s", session.message.text);
+    request(options, (err,resp,body) => {
+  		session.send(body);
+	})
 });
-/*
-var connector = new builder.ConsoleConnector().listen();
-var bot = new builder.UniversalBot(connector, function (session) {
-	session.send("You said: %s", session.message.text);
-});
-*/
